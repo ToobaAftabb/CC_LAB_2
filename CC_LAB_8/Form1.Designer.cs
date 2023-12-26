a44fad2e14b2e3a8fd9a0544ae040bed92ef52d3
ToobaAftabb <113778113+ToobaAftabb@users.noreply.github.com>
2023-12-26 11:21:27 +0500

    Add files via upload

---
+++ CC_LAB_8/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Compile_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(86, 62);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(100, 20);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "abcc";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(86, 103);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(100, 20);
+            this.Output.TabIndex = 1;
+            // 
+            // Compile_Click
+            // 
+            this.Compile_Click.Location = new System.Drawing.Point(86, 150);
+            this.Compile_Click.Name = "Compile_Click";
+            this.Compile_Click.Size = new System.Drawing.Size(100, 23);
+            this.Compile_Click.TabIndex = 2;
+            this.Compile_Click.Text = "button1";
+            this.Compile_Click.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Compile_Click);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Compile_Click;
+    }
+}
+
